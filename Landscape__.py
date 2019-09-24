641dc9f0fa32bfd9a52972882f53704998acab7a
100103067 <100103067@lubbockisd.net>
2019-09-24 15:35:26 -0500

    making house,tree,car,grass,

---
+++ Landscape__.py
@@ -0,0 +1,57 @@
+import turtle
+
+Ross = turtle.Turtle()
+Rachel = turtle.Turtle()
+Chandler = turtle.Turtle()
+Monica = turtle.Turtle()
+Phoebe = turtle.Turtle()
+
+Ross.shape("turtle")
+Rachel.shape("turtle")
+Chandler.shape("turtle")
+Monica.shape("turtle")
+Phoebe.shape("turtle")
+
+Ross.fillcolor("green")
+Ross.begin_fill()
+Ross.color("green")
+Ross.forward(800)
+Ross.backward(1800)
+Ross.right(90)
+Ross.forward(800)
+Ross.left(90)
+Ross.forward(1800)
+Ross.left(90)
+Ross.forward(800)
+Ross.end_fill()
+
+Rachel.fillcolor("pink")
+Rachel.begin_fill()
+Rachel.color("pink")
+
+def square(turtle,side):
+    for i in range(4):
+        Rachel.right(90)
+        Rachel.forward(100)
+
+Rachel.right(180)
+
+
+square(turtle, 75)
+Rachel.right(90)
+Rachel.forward(100)
+Rachel.end_fill()
+
+Rachel.fillcolor("purple")
+Rachel.begin_fill()
+Rachel.right(35)
+Rachel.forward(100)
+Rachel.right(117)
+Rachel.forward(98)
+Rachel.end_fill()
+
+Chandler.forward(300)
+
+Chandler.circle(15)
+
+turtle.exitonclick()
